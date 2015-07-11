b66cfd361f7780612400b94b9d4f412b7b7638c8
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2015-07-11 16:00:10 -0400

    Delete vim-coffee-script before installing

--- init.sh
+++ init.sh
@@ -36,6 +36,7 @@ git remote set-url origin git@github.com:lj-ditrapani/dotfiles.git
 cd ~
 
 # Vim setup
+rm -fr ~/.vim/bundle/vim-coffee-script
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
     curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/kchmck/vim-coffee-script.git ~/.vim/bundle/vim-coffee-script/
