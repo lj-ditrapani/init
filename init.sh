912c8d3d352b4093767d2bff7d478ad9965e24c6
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2015-07-11 11:23:35 -0400

    Move shell commands to init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 # TODO: uncomment update
 # sudo apt-get update
-sudo apt-get -y install puppet git
+sudo apt-get -y install puppet git curl
+curl -sL https://deb.nodesource.com/setup_0.12 | sudo /bin/bash -
 rm -fr ~/puppet
 git clone https://github.com/lj-ditrapani/puppet.git ~/puppet
 sudo /usr/bin/puppet apply ~/puppet/puppet.pp
+rm -rf ~/dotfiles
+git clone https://github.com/lj-ditrapani/dotfiles.git
+sh ~/dotfiles/link.rb'
