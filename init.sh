bb2af31d1180eeae3314554433fc6478babdc053
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2015-07-11 11:48:10 -0400

    Add a bunch more packages

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 # TODO: uncomment update
 # sudo apt-get update
+cd ~
 sudo apt-get -y install puppet git curl
 curl -sL https://deb.nodesource.com/setup_0.12 | sudo /bin/bash -
 rm -fr ~/puppet
@@ -7,4 +8,4 @@ git clone https://github.com/lj-ditrapani/puppet.git ~/puppet
 sudo /usr/bin/puppet apply ~/puppet/puppet.pp
 rm -rf ~/dotfiles
 git clone https://github.com/lj-ditrapani/dotfiles.git
-sh ~/dotfiles/link.rb'
+~/dotfiles/link.rb
--- puppet.pp
+++ puppet.pp
@@ -35,8 +35,76 @@ $packages = [
     'ri',
     'ruby2.1-doc',
 
+    # essential dev tools
     'curl',
     'git',
+    'silversearcher-ag',
+    'xsel',
+    'docker',
+    'tmux',
+    # for ssh sftp and scp access on sofi's pc
+    'openssh-server',
+
+    # media
+    'flashplugin-installer',
+    'vlc',
+    # bit-torrent
+    'transmission-gtk',
+
+    # rvm requirements
+    'gawk',
+    'libssl-dev',
+    'libyaml-dev',
+    'libyaml-dev',
+    'libsqlite3-dev',
+    'autoconf',
+    'libgdbm-dev',
+    'libncurses5-dev',
+    'automake',
+    'libtool',
+    'bison',
+    'pkg-config',
+
+    # Graphics
+    'gtypist',
+    'inkscape',
+    'mesa-utils',
+    'feh',
+    'eog',
+    'imagemagick',
+    'imagemagick-doc',
+
+    # More dev tools
+    'bvi',
+    'tidy',
+    'tidy-doc',
+    'dos2unix',
+    'sqlite3',
+    'sqlite3-doc',
+
+    # extract .rar, non-free
+    'unrar',
+    'p7zip-full',
+    'libreoffice-calc',
+
+    # Android
+    'android-tools-adb',
+    'android-tools-fastboot',
+    'mtpfs',
+
+    # Graphical dev
+    'gedit',
+    'lxterminal',
+
+    # More media
+    'mplayer2',
+    # to play SNES SPC files
+    'audacious',
+    # For firefox itunes streaming
+    'gstreamer1.0-libav',
+    'gstreamer1.0-plugins-bad',
+    # for TBN streaming
+    'gstreamer1.0-plugins-ugly',
 ]
 
 package { $packages:
