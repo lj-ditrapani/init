3d17ae34fcff758384e2a32910c5041a092c4612
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2015-07-11 10:15:22 -0400

    More packages

--- puppet.pp
+++ puppet.pp
@@ -7,6 +7,35 @@ $packages = [
     'zsh-lovers',
     'vim-doc',
     'vim-nox',
+    'wicd-curses',
+    'alsa-utils',
+
+    # X
+    'xorg',
+    'xterm',
+    'i3',
+    'synaptic',
+    'firefox',
+    'evince',
+    'xchm',
+    'build-essential',
+
+    # node
+    'nodejs',
+    'nodejs-dev',
+    'npm',
+
+    # python
+    'python3-doc',
+    'idle3',
+    'python3-examples',
+    'python3-pil',
+    'python3-pil.imagetk',
+
+    # ruby
+    'ruby-full',
+    'ri',
+    'ruby2.1-doc',
 ]
 
 package { $packages:
