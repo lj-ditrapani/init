f8dbbf507caf02aeee90fb56e6fa5bc32ad93e27
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2015-07-11 14:22:38 -0400

    Organize into sections and add vim plugin

--- init.sh
+++ init.sh
@@ -1,13 +1,18 @@
 sudo apt-get update
-cd ~
 sudo apt-get -y install puppet git curl
 curl -sL https://deb.nodesource.com/setup_0.12 | sudo /bin/bash -
+
+# puppet
 rm -fr ~/puppet
 git clone https://github.com/lj-ditrapani/puppet.git ~/puppet
 sudo /usr/bin/puppet apply ~/puppet/puppet.pp
+
+# dotfiles
 rm -rf ~/dotfiles
-git clone https://github.com/lj-ditrapani/dotfiles.git
+cd ~
+git clone https://github.com/lj-ditrapani/dotfiles.git ~/dotfiles
 ~/dotfiles/link.rb
+
 sudo systemctl stop sshd.service
 sudo systemctl disable sshd.service
 mkdir -p ~/local
@@ -16,6 +21,8 @@ mkdir -p ~/media
 mkdir -p ~/fun
 sudo npm install -g coffee-script jslint jshint coffeelint gulp
 sudo /usr/share/doc/libdvdread4/install-css.sh
+
+# git setup
 git config --global user.name "Lyall Jonathan Di Trapani"
 git config --global user.email lj.ditrapani@gmail.com
 git config --global core.editor vim
@@ -23,3 +30,8 @@ git config --global push.default simple
 cd ~/dotfiles
 git remote set-url origin git@github.com:lj-ditrapani/dotfiles.git
 cd ~
+
+# Vim setup
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+git clone https://github.com/kchmck/vim-coffee-script.git ~/.vim/bundle/vim-coffee-script/
