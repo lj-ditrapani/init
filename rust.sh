ae3c2720fbbb279df2574f746719125974d5ecb2
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2015-10-25 18:50:38 -0400

    Add more details for rust racer setup

--- rust.sh
+++ rust.sh
@@ -14,3 +14,7 @@ curl -L https://static.rust-lang.org/dist/rustc-1.3.0-src.tar.gz -o ~/local/rust
 tar -zxvf ~/local/rustc-1.3.0-src.tar.gz -C ~/local/
 
 # Uncomment rust racer vundle plugin in .vimrc
+# and rust racer config in .vimrc
+
+# Then run this
+vim +PluginInstall +qall
