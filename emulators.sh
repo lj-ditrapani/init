7f822484e818289bff877a13c1db6d6749c57c1c
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2015-10-25 16:37:53 -0400

    Add list of emulators

---
+++ emulators.sh
@@ -0,0 +1,14 @@
+sudo apt-get -y install \
+    mednafen \
+    stella \
+    vice \
+    fceux \
+    visualboyadvance-gtk \
+    higan \
+#    mednafen           snes, playstation, gba
+#    stella             atari 2600
+#    mess               atari 2600, commodore 64
+#    vice               commodore 64
+#    fceux              nes
+#    visualboyadvance-gtk    # gba
+#    higan                   # snes
