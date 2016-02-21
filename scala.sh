0f82014187a3efa596232ff3c4d163bc3ae11663
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2016-02-21 11:14:12 -0500

    Add global sbt install for scala

--- scala.sh
+++ scala.sh
@@ -14,3 +14,9 @@ curl https://raw.githubusercontent.com/n8han/conscript/master/setup.sh | sh
 
 # giter8
 cs n8han/giter8
+
+# sbt
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
+sudo apt-get update
+sudo apt-get install sbt
