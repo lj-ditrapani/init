31ad7c6047f9c3c5920ff9c54c14027ce7f19cb1
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2016-03-19 07:34:14 -0400

    Bump scala version & change typesafe to lightbend

--- scala.sh
+++ scala.sh
@@ -1,11 +1,11 @@
-version=2.11.7
+version=2.11.8
 scala_name=scala-${version}
 scala_tar=${scala_name}.tgz
 dir=~/local
 scala_tar_path=${dir}/${scala_tar}
 
 sudo apt-get -y install openjdk-8-jdk
-curl http://downloads.typesafe.com/scala/${version}/${scala_tar} > ${scala_tar_path}
+curl http://downloads.lightbend.com/scala/${version}/${scala_tar} > ${scala_tar_path}
 tar -xvf $scala_tar_path -C $dir
 mv ${dir}/${scala_name} ${dir}/scala
 
