f743899cc8099e7b2e18eb949d693223d94fb58d
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2016-10-15 12:03:19 -0400

    Update nvidia proprietary graphic drivers install

--- nvidia.sh
+++ nvidia.sh
@@ -1,3 +1,11 @@
 sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt-get update
-sudo apt-get install --no-install-recommends nvidia-352 nvidia-settings libcuda1-352 nvidia-opencl-icd-352 bumblebee-nvidia
+sudo apt-get install ubuntu-drivers-common
+# Run this to get the recommended binary version
+ubuntu-drivers devices
+# Replace 370 with the cerommended binary version
+sudo apt-get install --no-install-recommends nvidia-370 nvidia-settings libcuda1-370 nvidia-opencl-icd-370 bumblebee-nvidia
+sudo shutdown -h now
+
+# don't do this, installs the world
+# sudo ubuntu-drivers autoinstall
