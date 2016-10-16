d4ef9481907989a57e417140c3c38fb7d08147ad
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2016-10-15 20:19:48 -0400

    Update steam instructions

--- steam.sh
+++ steam.sh
@@ -1,6 +1,7 @@
 # Download steam_latest.deb from steam website
-curl media.steampowered.com/client/installer/steam.deb > ~/steam_latest.db
-python-apt xterm
+curl media.steampowered.com/client/installer/steam.deb > ~/steam.db
+sudo apt-get install python-apt xterm
 
+sudo dpkg -i steam.deb
 sudo apt-get install -f
 sudo dpkg -i steam.deb
