983fadb454f4afb66752efca65be14467e5ce33f
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2017-07-02 08:24:55 -0400

    docker

--- docker.sh
+++ docker.sh
@@ -1,10 +1,38 @@
-# Add official docker repo
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-# BRITTLE:  Only works with Ubuntu 16.04; must update ubuntu-xenial to correct version for later Ubuntu releases
-sudo sh -c "echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' >> /etc/apt/sources.list.d/docker.list"
+sudo apt-get install \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
 
-sudo apt-get install docker-engine
-sudo usermod -a -G docker ljd
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+# Add official docker repo
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Add the Docker repository to APT sources
+sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce
+
+sudo groupadd docker
+sudo usermod -aG docker ljd
 
 sudo systemctl enable docker
 sudo systemctl restart docker.service
+
+# Now log out!
+
+# To setup ubuntu to test with:
+docker pull ubuntu:16.04
+docker run ubuntu:16.04 /bin/bash
+apt-get update
+apt-get install wget sudo apt-transport-https
+adduser ljd
+usermod -aG sudo ljd
+su ljd
