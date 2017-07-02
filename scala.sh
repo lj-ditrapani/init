d3250d5c00185e52bc9465707caaee4b8901d0f2
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2017-07-01 21:12:10 -0400

    Update scala install

--- scala.sh
+++ scala.sh
@@ -1,4 +1,4 @@
-version=2.12.1
+version=2.12.2
 scala_name=scala-${version}
 scala_tar=${scala_name}.tgz
 dir=~/local
@@ -7,10 +7,3 @@ scala_tar_path=${dir}/${scala_tar}
 curl http://downloads.lightbend.com/scala/${version}/${scala_tar} > ${scala_tar_path}
 tar -xvf $scala_tar_path -C $dir
 mv ${dir}/${scala_name} ${dir}/scala
-
-# Conscript
-curl https://raw.githubusercontent.com/foundweekends/conscript/master/setup.sh | sh
-
-# giter8
-cs foundweekends/giter8
-g8
