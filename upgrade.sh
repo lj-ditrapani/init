515ecba76d8a7413a0e491d457aa65badb486fa8
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2018-07-21 18:04:04 -0400

    Add rust and remove acd_cli to upgrade

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo pip3 install --upgrade pip
-pip3 install --upgrade git+https://github.com/yadayada/acd_cli.git
 vim +PluginUpdate +qall
+rustup update
