c343cd94f68e092157d6374f4fb6394a4d299574
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2018-07-31 22:39:14 -0400

    Add coursier & scalafmt cmd line install

--- other/scala.sh
+++ other/scala.sh
@@ -7,3 +7,9 @@ scala_tar_path=${dir}/${scala_tar}
 curl http://downloads.lightbend.com/scala/${version}/${scala_tar} > ${scala_tar_path}
 tar -xvf $scala_tar_path -C $dir
 mv ${dir}/${scala_name} ${dir}/scala
+
+cd ~
+curl -L -o coursier https://git.io/vgvpD && chmod +x coursier
+sudo ./coursier bootstrap com.geirsson:scalafmt-cli_2.12:1.5.1 \
+  -r bintray:scalameta/maven \
+  -o /usr/local/bin/scalafmt --standalone --main org.scalafmt.cli.Cli
