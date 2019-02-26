df716169d52b77fe46487e53c6586619502bd90e
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-02-26 12:35:21 -0500

    Add nvm to upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -3,3 +3,6 @@ sudo apt-get upgrade
 sudo pip3 install --upgrade pip
 vim +PluginUpdate +qall
 rustup update
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+# nvm alias default <version#>
+# nvm alias default 10.15.1
