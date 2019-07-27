9f6f7313669381e15a7d991c84f3baea80fdae99
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-07-26 22:42:44 -0400

    virtualbox install

---
+++ virtualbox.sh
@@ -0,0 +1,5 @@
+wget -q -O - https://www.virtualbox.org/download/oracle_vbox_2016.asc | \
+    sudo apt-key add -
+echo "deb https://download.virtualbox.org/virtualbox/debian bionic contrib" | \
+    sudo tee -a /etc/apt/sources.list.d/virtualbox.list
+# sudo apt-get install virtualbox-6.0 virtualbox-ext-pack
