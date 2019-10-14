a0b0364bdc89a5386e1851d3e7e675f56268b079
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-10-14 18:36:37 -0400

    Update steam instructions

--- other/steam.sh
+++ other/steam.sh
@@ -1,7 +1,7 @@
 # Download steam_latest.deb from steam website
-curl http://repo.steampowered.com/steam/archive/precise/steam_latest.deb > ~/steam.deb
+curl http://repo.steampowered.com/steam/archive/precise/steam_latest.deb > ~/Downloads/steam.deb
 sudo apt-get install python-apt xterm
 
-sudo dpkg -i steam.deb
+sudo dpkg -i ~/Downloads/steam.deb
 sudo apt-get install -f
-sudo dpkg -i steam.deb
+sudo dpkg -i ~/Downloads/steam.deb
