40aa0fa7fb4e7af377bf137a9bba339d0cc842fd
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-12-14 09:32:59 -0500

    Update android studio install

--- androidstudio.sh
+++ androidstudio.sh
@@ -1,3 +1,4 @@
 # download tar from https://developer.android.com/studio
 sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
+sudo apt-get install qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils cpu-checker
 tar -zxvf $HOME/local/android-studio-ide-191.5977832-linux.tar.gz
