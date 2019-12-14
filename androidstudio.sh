71a5133f1aaf90615050804e33cf3550cb62ffcf
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-12-14 09:04:26 -0500

    android studio & swift install

---
+++ androidstudio.sh
@@ -0,0 +1,3 @@
+# download tar from https://developer.android.com/studio
+sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
+tar -zxvf $HOME/local/android-studio-ide-191.5977832-linux.tar.gz
