77f92140f54c60c1c1e753b63121ea84064229f3
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-12-14 09:56:57 -0500

    Add user to kvm group for android studio

--- androidstudio.sh
+++ androidstudio.sh
@@ -2,3 +2,4 @@
 sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
 sudo apt-get install qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils cpu-checker
 tar -zxvf $HOME/local/android-studio-ide-191.5977832-linux.tar.gz
+sudo adduser $USER kvm
