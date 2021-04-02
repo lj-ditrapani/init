53b4b19ac3c66c64e60a91c23c40af4d789f7320
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2021-04-02 10:56:34 -0400

    Use nvim not vim

--- init-workstation.sh
+++ init-workstation.sh
@@ -63,4 +63,4 @@ sudo systemctl mask systemd-networkd-wait-online.service
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugInstall +qall
+nvim +PlugInstall +qall
