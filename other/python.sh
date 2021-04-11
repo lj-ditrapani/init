89c9fbcbea80bfd9d192bb8e251603fbc7e81649
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2021-04-11 17:11:40 -0400

    Add python install instructions

---
+++ other/python.sh
@@ -0,0 +1,5 @@
+# Install python build dependencies
+sudo apt-get update; sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+curl https://pyenv.run | bash
+# pyenv install --list
+# pyenv install 3.9.4
