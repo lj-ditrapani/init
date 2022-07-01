ac5762d1f07f9907f052319fd758bf0b38476f9b
Jonathan Di Trapani <jonathan.ditrapani@statflo.com>
2022-07-01 13:22:07 -0400

    Add install virtualbox extension pack instructions

--- other/virtualbox.sh
+++ other/virtualbox.sh
@@ -4,3 +4,8 @@ echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] 
 wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmor --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg
 # sudo apt-get update
 # sudo apt-get install virtualbox-6.1
+
+# Extension pack
+# Download virtualbox extention from https://www.virtualbox.org/wiki/Downloads
+# https://download.virtualbox.org/virtualbox/6.1.34/Oracle_VM_VirtualBox_Extension_Pack-6.1.34.vbox-extpack
+# sudo vboxmanage extpack install Oracle_VM_VirtualBox_Extension_Pack-6.1.34.vbox-extpack
