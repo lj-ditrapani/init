47a3ab2b7a3d510a9a468b32473b01a9b4499d92
Jonathan Di Trapani <jonathan.ditrapani@statflo.com>
2022-07-01 12:58:50 -0400

    Update virtualbox install

--- other/virtualbox.sh
+++ other/virtualbox.sh
@@ -1,5 +1,6 @@
-wget -q -O - https://www.virtualbox.org/download/oracle_vbox_2016.asc | \
-    sudo apt-key add -
-echo "deb https://download.virtualbox.org/virtualbox/debian bionic contrib" | \
-    sudo tee -a /etc/apt/sources.list.d/virtualbox.list
-# sudo apt-get install virtualbox-6.0
+# Update distro (hirsute) to whatever (jammy)
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian hirsute contrib" \
+    | sudo tee -a /etc/apt/sources.list.d/virtualbox.list
+wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmor --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg
+# sudo apt-get update
+# sudo apt-get install virtualbox-6.1
