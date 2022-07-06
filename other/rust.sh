1663f6aa43a8255893367a06b0d8d5323efe8d9b
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2022-07-05 20:57:56 -0400

    Remove other install scripts
    These have either been integrated into main install script
    (nvim, rust, scala)
    or are not used/outdated.

--- other/rust.sh
+++
@@ -1,2 +0,0 @@
-# Install (or upgrade) rustc
-curl -L https://sh.rustup.rs | sh
--- other/swift.sh
+++
@@ -1,3 +0,0 @@
-# Download Ubuntu Toolchain: https://swift.org/download/#releases
-sudo apt-get install clang libicu-dev
-tar -zxvf $HOME/local/swift-5.1.2-RELEASE-ubuntu18.04.tar.gz
