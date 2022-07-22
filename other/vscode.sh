a58e1d8944c363ede682e79ac1518e324bb04491
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2022-07-22 00:47:58 -0400

    Add vscode alias to vscode notes

--- other/vscode.sh
+++ other/vscode.sh
@@ -1,6 +1,8 @@
 curl -L "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" > ~/Downloads/vscode.deb
 sudo dpkg -i ~/Downloads/vscode.deb
 
+vscode
+# which is aliased in .zshrc to:
 code --enable-features=UseOzonePlatform --ozone-platform=wayland --log debug
 # install extensions
 # - vim
