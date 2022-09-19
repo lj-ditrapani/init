9e5f9c534b2d7f211cfd7169e360cbdcd40c1aea
Jonathan Di Trapani <jonathan.ditrapani@statflo.com>
2022-09-19 19:10:48 -0400

    Add dbeaver install instructions

---
+++ other/dbeaver.sh
@@ -0,0 +1,6 @@
+sudo add-apt-repository ppa:serge-rider/dbeaver-ce
+sudo apt-get update
+sudo apt-get install dbeaver-ce
+
+# curl -l 'https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb' > ~/Downloads/dbeaver.deb
+# sudo dpkg -i ~/Downloads/dbeaver.deb
