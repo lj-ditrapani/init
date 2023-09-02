52d76e45962371f38aa13331c9e5b846295b8d3f
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2023-09-02 18:46:01 -0400

    Add header files (*-dev deb packages) for building python
    This is needed so pyenv can build python installs.

--- install-workstation.sh
+++ install-workstation.sh
@@ -50,6 +50,19 @@ sudo apt-get -y install \
     docker-ce-cli \
     docker-compose-plugin \
 \
+    libssl-dev \
+    zlib1g-dev \
+    libbz2-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    libncursesw5-dev \
+    xz-utils \
+    tk-dev \
+    libxml2-dev \
+    libxmlsec1-dev \
+    libffi-dev \
+    liblzma-dev \
+\
     mesa-utils \
     feh \
     eog \
