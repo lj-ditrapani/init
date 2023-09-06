dc0530727025ff614d0a72ec25f2b4b73536f926
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2023-09-05 21:27:26 -0400

    feat: add v-user readme & init

---
+++ v-init.sh
@@ -0,0 +1,39 @@
+# setup user
+sudo chsh -s /usr/bin/zsh $USER
+sudo usermod -aG audio,video,netdev $USER
+sudo usermod -aG docker $USER
+
+# dotfiles
+sudo rm -rf ~/dotfiles
+git clone https://github.com/lj-ditrapani/dotfiles.git ~/dotfiles
+git checkout vbranch
+~/dotfiles/link.rb
+cd ~
+
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+sudo ln -s $HOME/.local/kitty.app/bin/kitty /usr/bin/kitty
+
+mkdir -p ~/local
+mkdir -p ~/usb
+mkdir -p ~/media
+mkdir -p ~/fun
+mkdir -p ~/Downloads
+mkdir -p ~/tmp
+
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install java 17.0.3-tem
+sdk install gradle
+sdk install sbt
+sdk install scala
+sdk install kotlin
+sdk install kscript
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install --lts
+
+curl https://pyenv.run | bash
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
