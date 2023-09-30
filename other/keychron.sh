de87f653a0c53b3f76815fdc8f1b94199d6a41f6
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2023-09-30 00:52:17 -0400

    docs: [keyboard] and via instructions & keymap

--- other/keychron.sh
+++ other/keychron.sh
@@ -1,4 +1,4 @@
-# How to get function keys working with keychron
+# How to get function keys working with keychron C2
 
 echo 0 | sudo tee /sys/module/hid_apple/parameters/fnmode
 # test f keys; do they work?
@@ -8,3 +8,11 @@ echo 0 | sudo tee /sys/module/hid_apple/parameters/fnmode
 # to make premanant
 echo "options hid_apple fnmode=0" | sudo tee -a /etc/modprobe.d/hid_apple.conf
 sudo update-initramfs -u
+
+# using via with keychron V4
+# got to usevia.app (disable vimium)
+# check chrome://device-log and examine errors
+# to get hid device /dev/hidraw<n>
+sudo chmod a+rw /dev/hidraw<n>
+# reload
+sudo chmod go-rw /dev/hidraw<n>
