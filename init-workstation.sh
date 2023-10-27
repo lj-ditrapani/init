13db67d295422fcaa6623e3a298c5ab02fb64f88
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2023-10-27 19:37:01 -0400

    feat: remove i3 repo and install curl first
    Ubuntu desktop doesn't have curl by default, so let's install it first
    so we can use it to download other stuff to install.
    
    i3 latest release in available in ubuntu 23.10.

--- init-workstation.sh
+++ init-workstation.sh
@@ -1,8 +1,5 @@
-# instructions: https://i3wm.org/docs/repositories.html
-/usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2023.02.18_all.deb keyring.deb SHA256:a511ac5f10cd811f8a4ca44d665f2fa1add7a9f09bef238cdfad8461f5239cc4
-sudo apt install ./keyring.deb
-echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
-rm ./keyring.deb
+sudo apt-get update
+sudo apt-get install curl
 
 sudo mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
