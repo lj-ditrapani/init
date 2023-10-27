5cc64520e983be139afd41f0ce092c96a3c024be
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2023-10-27 19:57:18 -0400

    chore: organize keyboard stuff

--- other/keyboard-layouts/keychron-c2.sh
+++ other/keyboard-layouts/keychron-c2.sh
@@ -8,11 +8,3 @@ echo 0 | sudo tee /sys/module/hid_apple/parameters/fnmode
 # to make premanant
 echo "options hid_apple fnmode=0" | sudo tee -a /etc/modprobe.d/hid_apple.conf
 sudo update-initramfs -u
-
-# using via with keychron V4
-# got to usevia.app (disable vimium)
-# check chrome://device-log and examine errors
-# to get hid device /dev/hidraw<n>
-sudo chmod a+rw /dev/hidraw<n>
-# reload
-sudo chmod go-rw /dev/hidraw<n>
