909816ba4b6af145a639f39b09f2fb6e3f0884b6
Jonathan Di Trapani <jonathan.ditrapani@statflo.com>
2023-11-15 10:50:13 -0500

    feat: [pyenv] add pyenv to upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -8,6 +8,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 nvm install --lts
 # nvm alias default <version#>
+pyenv update
 sdk update
 sdk selfupdate
 sdk upgrade
