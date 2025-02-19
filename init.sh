17ec5b7c4b8d79f7fd8ac50ec5a7ecb1a26975f0
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2025-02-19 08:45:07 -0500

    feat: install intellij

--- init.sh
+++ init.sh
@@ -86,6 +86,7 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 sudo update-alternatives --set pinentry /usr/bin/pinentry-gtk-2
 sudo update-alternatives --set editor /usr/bin/nvim
+sudo snap install intellij-idea-community --classic
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall +qall
