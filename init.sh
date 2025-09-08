8ef221b7697007d217a6beb4bd9272e8dc65f59e
Lyall Jonathan Di Trapani <jonathan.ditrapani@assured.claims>
2025-09-08 17:12:39 -0400

    feat: trim install process to essentials
    remove ide installs and optional programming languages.

--- init.sh
+++ init.sh
@@ -9,11 +9,6 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub \
-  | sudo gpg  --dearmor -o /usr/share/keyrings/dart.gpg
-echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' \
-  | sudo tee /etc/apt/sources.list.d/dart_stable.list
-
 # install packages
 sudo apt-get update
 sudo apt-get -y upgrade
@@ -45,53 +40,19 @@ mkdir -p ~/fun
 mkdir -p ~/Downloads
 mkdir -p ~/tmp
 
-curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk install java 23.0.2-tem
-sdk install gradle
-sdk install sbt
-sdk install scala
-sdk install kotlin
-
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install --lts
 
-curl -fsSL https://pyenv.run | bash
-curl -LsSf https://astral.sh/uv/install.sh | sh
-
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-
-mkdir -p ~/.local/bin
-# ziglang can be crazy slow
-# "https://ziglang.org/download/0.14.0/zig-linux-x86_64-0.14.0.tar.xz"
-# Using mirror instead
-curl -L "https://zigmirror.nesovic.dev/zig/zig-linux-x86_64-0.15.0-dev.10+214750fcf.tar.xz" -o ~/.local/zig-0.15.0.tar.xz
-cd ~/.local
-tar -xf zig-0.15.0.tar.xz
-rm -f $HOME/.local/bin/zig
-ln -s $HOME/.local/zig-linux-x86_64-0.15.0-dev.10+214750fcf/zig $HOME/.local/bin
-cd
 
 chmod 700 ~/.gnupg
 sudo apt purge cloud-init -y
 sudo rm -fr /etc/cloud && sudo rm -rf /var/lib/cloud/
 
-# don't run as root
-opam init -y
-eval $(opam env --switch=default)
-opam install ocaml-lsp-server odoc ocamlformat utop merlin
-
-curl -L "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" > ~/Downloads/vscode.deb
-sudo dpkg -i ~/Downloads/vscode.deb
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-
-
 sudo update-alternatives --set pinentry /usr/bin/pinentry-gtk-2
 sudo update-alternatives --set editor /usr/bin/nvim
-sudo snap install intellij-idea-community --classic
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall +qall
