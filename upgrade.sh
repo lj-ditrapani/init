a03593799c5b47550b2f0a7af504fc5fb7e1f7be
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2025-12-12 08:53:50 -0500

    feat: update upgrade script
    Remove lines for apps that are now managed by homebrew.

--- upgrade.sh
+++ upgrade.sh
@@ -3,14 +3,10 @@ sudo apt-get upgrade
 # To update kernel (check packages selected are ok)
 # sudo do-release-upgrade
 # sudo apt install update-manager-core # povides do-release-upgrade
-vim +PluginUpdate +qall
+# brew upgrade ?
+# brew update ?
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
-rustup update
-pyenv update
-uv self update
 sdk update
 sdk selfupdate
 sdk upgrade
-opam update
 sudo snap refresh
